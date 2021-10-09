55de3389a4603e3fc3452a6fb4fa7eada9cf2d7b
lakhwindersingh <lakhwindersingh@yahoo.com>
2021-10-09 10:25:08 -0400

    included uvicorn server and fastAPI for rest services

--- recipe/fin_ml/entrypoint.sh
+++ recipe/fin_ml/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run Jupyterlab
-#cd /home/jupyter/notebook
-#sudo runuser -l jupyter -c "/opt/conda/bin/jupyter lab --port=9999 --ip=0.0.0.0 --no-browser"
-sudo uvicorn --reload-dir /home/jupyter/notebook/neutron_binary/py_code/fastapi/ --port 9292 main.py:app --reload  & \
+#sudo PYTHONPATH=/home/jupyter/notebook/neutron_binary/py_code/fastapi/ uvicorn main.app:application & \
+sudo uvicorn --host 0.0.0.0 --port 9292 --app-dir /home/jupyter/notebook/neutron_binary/py_code/fastapi/ main:app --reload  & \
+
 sudo runuser -l jupyter -c "/home/jupyter/.local/bin/jupyter lab --allow-root --port=8282 --ip=0.0.0.0 --no-browser"
